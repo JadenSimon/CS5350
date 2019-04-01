98afb42c6649e007afce53e62341e54da9a54c1d
JadenSimon <simonjaden223@gmail.com>
2019-03-31 18:57:57 -0600

    Added Dual SVM

--- HW4/hw4_test.py
+++ HW4/hw4_test.py
@@ -8,7 +8,7 @@ sys.path.append('../SVM')
 import numpy as np
 import decision_tree as DT
 import perceptron as PT
-import svm as SVM
+import SVM as SVM
 
 np.set_printoptions(precision=3, suppress=True, threshold=np.nan)
 
@@ -18,9 +18,11 @@ bank_test = np.array(DT.import_data("bank-note/test.csv"), dtype='float64')
 
 # Problem 2 stuff
 C = [1.0/873, 10.0/873, 50.0/873, 100.0/873, 500.0/873, 700.0/873]
+C_Dual = [100.0/873, 500.0/873, 700.0/873]
+y_Dual = [0.01, 0.1, 0.5, 1.0, 2.0, 5.0, 10.0, 100.0]
 
 # For problem 2a
-d = 10
+d = 0.001
 def r_func1(r, t): 
 	return r / (1 + ((r/d) * t)) 
 
@@ -28,8 +30,89 @@ def r_func1(r, t):
 def r_func2(r, t):
 	return r / (1 + t)
 
+# Returns the average number of prediction errors using a kernel
+def kernel_error(alphs, sv, b, data, k):
+	errors = 0.0
+
+	# Go over every example
+	for example in data:
+		x = example[:-1]
+		y = example[-1]
+
+		if y == 0:
+			y = -1
+
+		t = 0
+		for i in range(len(sv)):
+			t += alpha[i] * sv[i][-1] * k(sv[i][:-1], x)
+
+		if y * (t + b) <= 0:
+			errors += 1.0
+
+	return errors / data.shape[0]
+
+# Returns a list of support vectors from alpha
+def get_sv(alpha, data, tol):
+	sv = np.array(data)
+	new_alpha = np.array(alpha)
+	for i in range(len(alpha)):
+		if alpha[i] < tol:
+			sv = np.delete(sv, data[i], axis=1)
+			new_alpha = np.delete(new_alpha, alpha[i])
+
+	return new_alpha, sv
+
+def count_sv(alpha, sv, tol):
+	c = 0
+	for i in range(len(alpha)):
+		if alpha[i] > tol:
+			c += 1
+
+	return c
+
+# Get the bias using alpha and sv
+def get_bias(alpha, sv, k):
+	b = 0
+
+	for i in range(len(alpha)):
+		for j in range(len(alpha)):
+			b -= alpha[j] * sv[j][-1] * k(sv[i][:-1], sv[j][:-1])
+
+		b += sv[i][-1]
+
+	return b / len(alpha)
+
+print "Problem 3a (Dual SVM)"
+k = lambda x, y: np.dot(x, y) # Linear kernel
+alpha = SVM.dual_svm(bank_examples, k, 100, 700.0/873, False)
+#alpha, sv = get_sv(alpha, bank_examples, 1e-6)
+sv = bank_examples
+b = get_bias(alpha, sv, k)
+print "\tTraining Error: " + str(kernel_error(alpha, sv, b, bank_examples, k) * 100) + "%"
+print "\tTesting Error: " + str(kernel_error(alpha, sv, b, bank_test, k) * 100) + "%"
+
+print "Problem 3b (Dual SVM with Gaussian)"
+for c in C_Dual:
+	for y in y_Dual:
+		k = lambda x1, x2: np.exp(-(np.linalg.norm(x1 - x2)**2) / y)
+		alpha = SVM.dual_svm(bank_examples, k, 100, c, False)
+		sv = bank_examples
+		b = get_bias(alpha, sv, k)
+		print "C = " + str(c) + " : gamma = " + str(y) + " : SV Count = " + str(count_sv(alpha, sv, 1e-3))
+		print "\tTraining Error: " + str(kernel_error(alpha, sv, b, bank_examples, k) * 100) + "%"
+		print "\tTesting Error: " + str(kernel_error(alpha, sv, b, bank_test, k) * 100) + "%"
+
+
 print "Problem 2a (Sub-gradient Descent SVM)"
 for c in C:
-	w = SVM.perceptron(bank_examples, 1.0, r_func1, 100, c, False)
-	print "C = " + str(c) + ": " + str(w)
-	print "\tTraining Error: " + str(PT.avg_prediction_error(w, bank_examples * 100) + "%"" Testing Error: " + str(PT.avg_prediction_error(w, bank_test) * 100) + "%""
+	w = SVM.sgd_svm(bank_examples, 0.001, r_func1, 100, c, False)
+	print "C = " + str(c) + ": w = " + str(w)
+	print "\tTraining Error: " + str(PT.avg_prediction_error(w, bank_examples) * 100) + "%"
+	print "\tTesting Error: " + str(PT.avg_prediction_error(w, bank_test) * 100) + "%"
+
+print "Problem 2b (Sub-gradient Descent SVM)"
+for c in C:
+	w = SVM.sgd_svm(bank_examples, 0.001, r_func2, 100, c, False)
+	print "C = " + str(c) + ": w = " + str(w)
+	print "\tTraining Error: " + str(PT.avg_prediction_error(w, bank_examples) * 100) + "%"
+	print "\tTesting Error: " + str(PT.avg_prediction_error(w, bank_test) * 100) + "%"
--- SVM/SVM.py
+++ SVM/SVM.py
@@ -4,6 +4,7 @@
 # Description: Implements various Support Vector Machine algorithms
 
 import numpy as np
+from scipy.optimize import minimize
 
 # Performs sub-gradient descent for SVM. r0 is the initial learning rate while r_func is a function used to 
 #change the learning rate every epoch
@@ -11,6 +12,7 @@ def sgd_svm(data, r0, r_func, T, C, print_data):
 	data = np.array(data) # Make a copy because we will be shuffling it
 
 	w = np.zeros(data.shape[1], dtype='float64')
+	last_grad = w
 
 	# Maximum of T epochs
 	for t in range(T):
@@ -28,14 +30,75 @@ def sgd_svm(data, r0, r_func, T, C, print_data):
 
 			# Compute the gradient
 			grad = w
-			if max(0, 1 - y * np.dot(w, x)) == 0:
-				grad = grad - (C * data.shape[0] * y * x)
+			if max(0, 1 - y * np.dot(w, x)) != 0:
+				grad = grad - (C * y_data.shape[0] * y * x)
 
 			# Update w
 			w = w - (r_func(r0, t) * grad)
+
+			# Check for convergence
+			if np.linalg.norm(last_grad - grad) < (0.000001 * t):
+				return w
+
+			last_grad = grad
 
 		# Print out extra data
 		if print_data:
 			print "Step " + str(t) + ": w = " + str(w)
 
 	return w
+
+# SVM in the dual domain. Allows for kernels. Returns the weights of all data points.
+def dual_svm(data, k, T, C, print_data):
+	x_data = data[:,:-1]
+	y_data = data[:,-1]
+	y_data[y_data == 0] = -1 # Convert 0s to -1
+
+	# Randomly initialize weights
+	#a0 = np.random.random_sample(data.shape[0]) * C
+	a0 = np.zeros(data.shape[0])
+
+	# Create a multiplication factor
+	mf = np.zeros((data.shape[0], data.shape[0]))
+
+	for i in range(len(a0)):
+		for j in range(len(a0)):
+			mf[i][j] = y_data[i] * y_data[j] * k(x_data[i], x_data[j])
+
+	# Define our optimzation function
+	def optim_f(a):
+		y = 0
+		total_a = 0
+
+		for i in range(len(a)):
+			total_a += a[i]
+
+			for j in range(len(a)):
+				y += mf[i][j] * a[i] * a[j]
+
+		return (0.5 * y) - total_a
+
+	# Define the jacobian matrix
+	def optim_jac(a):
+		jac = np.zeros(data.shape[0])
+
+		for i in range(len(jac)):
+			for j in range(len(jac)):
+				if i != j:
+					jac[i] += (0.5) * mf[i][j] * a[j]
+
+			jac[i] += (a[i] * mf[i][i]) - 1
+
+		return jac
+
+	# Define bounds
+	bnds = [(0, C)] * len(a0)
+
+	# Define constraints
+	cons = ({'type': 'eq', 'fun': lambda a: np.dot(a, y_data)})
+
+	# Run the optimization
+	res = minimize(optim_f, a0, method='SLSQP', bounds=bnds, constraints=cons, jac=optim_jac, tol=1e-3, options={'maxiter': T, 'disp': print_data})
+
+	return res.x
+
